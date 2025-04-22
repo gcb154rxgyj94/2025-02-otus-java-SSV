a2995516c4e22ee1fbf7b6824aece08428fb6603
svyatoslav.stolbov <svyatoslav.stolbov@vk.team>
2025-04-22 11:17:41 +0300

    fix

--- hw05-bytecode/src/main/java/ru/otus/classes/Ioc.java
+++ hw05-bytecode/src/main/java/ru/otus/classes/Ioc.java
@@ -7,33 +7,40 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Ioc {
 
-    private static TestLoggingInterface testLogging = null;
+    private static TestLoggingInterface testLoggingProxy = null;
 
     private Ioc() {}
 
     public static TestLoggingInterface createMyClass() {
-        if (Objects.isNull(testLogging)) {
+        if (Objects.isNull(testLoggingProxy)) {
             TestLoggingInterface original = new TestLogging();
             InvocationHandler handler = new LogInvocationHandler(original);
-            testLogging = (TestLoggingInterface) Proxy.newProxyInstance(original.getClass().getClassLoader(), original.getClass().getInterfaces(), handler);
+            testLoggingProxy = (TestLoggingInterface) Proxy.newProxyInstance(original.getClass().getClassLoader(), original.getClass().getInterfaces(), handler);
         }
-        return testLogging;
+        return testLoggingProxy;
     }
 
     private static class LogInvocationHandler implements InvocationHandler {
 
-        private final TestLoggingInterface testLogging;
+        private final Object object;
+        private Set<Method> methods;
 
-        private LogInvocationHandler(TestLoggingInterface testLogging) {
-            this.testLogging = testLogging;
+        private LogInvocationHandler(Object object) {
+            this.object = object;
+            this.methods = Arrays.stream(object.getClass().getMethods()).collect(Collectors.toSet());
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            Method targetMethod = testLogging.getClass().getMethod(method.getName(), method.getParameterTypes());
+            Method targetMethod = methods.stream()
+                    .filter(method1 -> method1.getName().equals(method.getName()) && Arrays.equals(method1.getParameterTypes(), method.getParameterTypes()))
+                    .findFirst()
+                    .orElseThrow(() -> new ClassNotFoundException("Не найден метод " + method.getName() + " с параметрами " + Arrays.toString(method.getParameterTypes())));
             if (targetMethod.isAnnotationPresent(Log.class)) {
                 System.out.print("executed method: " + method.getName() + ", params: ");
                 Arrays.stream(args)
@@ -42,7 +49,7 @@ public class Ioc {
                         );
                 System.out.println();
             }
-            return method.invoke(testLogging, args);
+            return method.invoke(object, args);
         }
 
     }
