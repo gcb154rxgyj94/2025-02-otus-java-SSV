681065a79de27b281dd07723493171793a117898
svyatoslav.stolbov <svyatoslav.stolbov@vk.team>
2025-05-08 18:09:45 +0300

    написал тесты

--- hw07-patterns/src/main/java/ru/otus/processor/homework/ProcessorGenerateException.java
+++ hw07-patterns/src/main/java/ru/otus/processor/homework/ProcessorGenerateException.java
@@ -1,16 +1,20 @@
 package ru.otus.processor.homework;
 
+import lombok.AllArgsConstructor;
 import ru.otus.model.Message;
 import ru.otus.processor.Processor;
 
 import java.rmi.UnexpectedException;
 import java.time.LocalTime;
 
+@AllArgsConstructor
 public class ProcessorGenerateException implements Processor {
+
+    final TimeProvider timeProvider;
 
     @Override
     public Message process(Message message) {
-        if (LocalTime.now().getSecond() % 2 == 0) {
+        if (timeProvider.getTime().getSecond() % 2 == 0) {
             throw new RuntimeException("Exception on even second");
         }
         return message;
