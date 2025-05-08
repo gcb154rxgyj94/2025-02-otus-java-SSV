dcb3303227f1106e70a1aa1577edfae8e9b9a0a0
svyatoslav.stolbov <svyatoslav.stolbov@vk.team>
2025-05-08 18:26:43 +0300

    реализовал main

--- hw07-patterns/src/main/java/ru/otus/HomeWork.java
+++ hw07-patterns/src/main/java/ru/otus/HomeWork.java
@@ -1,24 +1,50 @@
 package ru.otus;
 
+import lombok.extern.slf4j.Slf4j;
+import ru.otus.handler.ComplexProcessor;
+import ru.otus.listener.homework.HistoryListener;
+import ru.otus.model.Message;
+import ru.otus.model.ObjectForMessage;
+import ru.otus.processor.LoggerProcessor;
+import ru.otus.processor.Processor;
+import ru.otus.processor.homework.ProcessorGenerateException;
+import ru.otus.processor.homework.ProcessorReplaceFields;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 public class HomeWork {
 
-    /*
-    Реализовать to do:
-      1. Добавить поля field11 - field13 (для field13 используйте класс ObjectForMessage)
-      2. Сделать процессор, который поменяет местами значения field11 и field12
-      3. Сделать процессор, который будет выбрасывать исключение в четную секунду (сделайте тест с гарантированным результатом)
-            Секунда должна определяьться во время выполнения.
-            Тест - важная часть задания
-            Обязательно посмотрите пример к паттерну Мементо!
-      4. Сделать Listener для ведения истории (подумайте, как сделать, чтобы сообщения не портились)
-         Уже есть заготовка - класс HistoryListener, надо сделать его реализацию
-         Для него уже есть тест, убедитесь, что тест проходит
-    */
-
     public static void main(String[] args) {
-        /*
-          по аналогии с Demo.class
-          из элеменов "to do" создать new ComplexProcessor и обработать сообщение
-        */
+        List<Processor> processors = List.of(new ProcessorReplaceFields(), new LoggerProcessor(new ProcessorGenerateException(LocalTime::now)));
+
+        ComplexProcessor complexProcessor = new ComplexProcessor(processors, ex -> {
+            log.info(ex.getMessage());
+        });
+        HistoryListener historyListener = new HistoryListener();
+        complexProcessor.addListener(historyListener);
+
+        ObjectForMessage field13 = new ObjectForMessage();
+        ArrayList<String> field13Data = new ArrayList<>();
+        field13Data.add("field13_1");
+        field13Data.add("field13_2");
+        field13.setData(field13Data);
+        Message message = new Message.Builder(1L)
+                .field1("field1")
+                .field2("field2")
+                .field3("field3")
+                .field6("field6")
+                .field10("field10")
+                .field11("field11")
+                .field12("field12")
+                .field13(field13)
+                .build();
+        log.info("result:{}", complexProcessor.handle(message));
+        Message message1 = historyListener.findMessageById(message.getId()).get();
+        log.info("result:{}", message1);
+        log.info("result:{}", complexProcessor.handle(message1));
+        complexProcessor.removeListener(historyListener);
     }
 }
