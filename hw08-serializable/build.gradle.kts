9ebb37fc75361befc970e8d7a30635e7a0c2aefc
svyatoslav.stolbov <svyatoslav.stolbov@vk.team>
2025-05-08 19:33:14 +0300

    сделал

--- hw08-serializable/build.gradle.kts
+++ hw08-serializable/build.gradle.kts
@@ -7,10 +7,15 @@ dependencies {
     testAnnotationProcessor ("org.projectlombok:lombok")
 
     implementation ("ch.qos.logback:logback-classic")
+    implementation ("com.fasterxml.jackson.core:jackson-databind:2.15.2")
 
     testImplementation ("org.junit.jupiter:junit-jupiter-api")
     testImplementation ("org.junit.jupiter:junit-jupiter-engine")
     testImplementation ("org.assertj:assertj-core")
     testImplementation ("org.mockito:mockito-junit-jupiter")
 
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
--- hw08-serializable/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw08-serializable/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,16 +1,38 @@
 package ru.otus.dataprocessor;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collections;
 import java.util.List;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import lombok.extern.slf4j.Slf4j;
 import ru.otus.model.Measurement;
 
+@Slf4j
 public class ResourcesFileLoader implements Loader {
 
-    public ResourcesFileLoader(String fileName) {}
+    private static final ObjectMapper mapper = JsonMapper.builder().build();
+    private static final ClassLoader classLoader = ResourcesFileLoader.class.getClassLoader();
+    private final String resourcesFile;
+
+    public ResourcesFileLoader(String fileName) {
+        resourcesFile = fileName;
+    }
 
     @Override
     public List<Measurement> load() {
-        // читает файл, парсит и возвращает результат
-        return Collections.emptyList();
+        try (InputStream inputStream = classLoader.getResourceAsStream(resourcesFile)) {
+            if (inputStream == null) {
+                log.error("Файл не найден: " + resourcesFile);
+                return Collections.emptyList();
+            }
+            return mapper.readValue(inputStream, new TypeReference<>() {});
+        } catch (IOException e) {
+            log.error("Ошибка при работе с потоками из файла " + resourcesFile + ": " + e.getMessage(), e);
+            throw new FileProcessException(e);
+        }
     }
 }
